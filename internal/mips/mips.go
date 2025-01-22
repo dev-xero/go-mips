48e7150d44a5ef865071c5f3a19c3d3828e0bcc7
= <ssh.xero@gmail.com>
2025-01-22 22:55:20 +0100

    chore: Define base CPU abstraction

--- internal/mips/mips.go
+++ internal/mips/mips.go
@@ -1,1 +1,19 @@
 package mips
+
+type Register uint32
+
+/* 
+	CPU abstraction
+	
+	Registers - 32 32-bit registers as per MIPS spec.
+	PC - Program Counter.
+	Memory - Simulated 1MB of memory.
+	HI, LO - 32 bit.
+
+*/
+type CPU struct {
+	Registers [32]Register
+	PC        uint32
+	Memory    []byte
+	HI, LO    Register
+}
