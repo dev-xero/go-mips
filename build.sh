99f1f525f69e991bf080b0386ef0bf0624aefbf9
= <ssh.xero@gmail.com>
2025-01-22 17:29:48 +0100

    init: Setup go environment

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Format all Go files
+echo "> Formatting Go code..."
+go fmt ./...
+
+# Formatting should succeed
+if [ $? -ne 0 ]; then
+    echo "[x] Formatting failed, terminating."
+    exit 1
+fi
+
+# Build the project
+echo "> Building the project..."
+go build -o ./bin/mips-sim ./cmd/mips/main.go
+
+# Check if the build was successful
+if [ $? -eq 0 ]; then
+    echo -e "> Build successful. Running the simulator...\n"
+    ./bin/mips-sim
+else
+    echo "[x] Build failed."
+fi
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dev-xero/go-mips
+
+go 1.23.4
