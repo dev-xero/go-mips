25b3183ee654289250be6002c235fc3cac56901f
= <ssh.xero@gmail.com>
2025-08-31 12:52:47 +0100

    chore: Improve server.sh
    Signed-off-by: = <ssh.xero@gmail.com>

--- hack/server.sh
+++ hack/server.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
-# Format all Go files
 echo "> Formatting Go code..."
 go fmt ./...
 
-# Formatting should succeed
 if [ $? -ne 0 ]; then
     echo "[x] Formatting failed, terminating."
     exit 1
 fi
 
-# Compile the server
 echo "> Compiling sever..."
 go build -o ./bin/server ./cmd/server/server.go
 
-# Check if the build was successful
 if [ $? -eq 0 ]; then
     echo -e "> Build successful. Running the server...\n"
     ./bin/server
